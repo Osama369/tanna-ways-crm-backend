37106835333892b8a748fff494d29e4981962db6
arayanmemon <memonaryu@gmail.com>
2024-07-28 20:53:25 +0500

    chore: Update package.json to use ES modules and fix MongoDB connection URL

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const ConnectDB = async () => {
     try {
-        const options = await mongoose.connect(`${process.env.MONGO_URL}/test`)
+        const options = await mongoose.connect(`${process.env.MONGO_URI}/test`)
         console.log("db connected "+ options.connection.host);
     } catch (error) {
         console.log("Error db FAILED: "+error);
